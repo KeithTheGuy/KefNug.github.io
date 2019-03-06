eb24169877b66ab55c30a0529586703986687452
zyp <15352128+zzypp@users.noreply.github.com>
2019-03-06 10:50:26 -0600

    Update and rename js/setContentHeight.js to zyp.js

--- js/setContentHeight.js
+++
@@ -1,9 +0,0 @@
-function setContentHeight() {     
-    setInterval(function(){
-            var divHeight = (window.innerHeight) - (document.getElementById("center").offsetTop) + 256;
-            var contentHeight = divHeight - 128;
-
-            document.getElementById("center").style.height = divHeight;
-            document.getElementById("content").style.height = contentHeight;
-    }, 100);
-}
---
+++ zyp.js
@@ -0,0 +1,22 @@
+function animatedTitle() {     
+    var animatedTitle = ["Zyp","zYp","zyP"];
+    var i = 0;    
+    setInterval(function(){
+        setInterval(function(){
+            document.title = animatedTitle[i];
+            i++;
+            if(i >= 2){
+                i = 0;
+            }
+        }, 512);
+    }, 1536);
+}
+function setContentHeight() {     
+    setInterval(function(){
+        var divHeight = (window.innerHeight) - (document.getElementById("center").offsetTop) + 256;
+        var contentHeight = divHeight - 128;
+
+        document.getElementById("center").style.height = divHeight;
+        document.getElementById("content").style.height = contentHeight;
+    }, 100);
+}
